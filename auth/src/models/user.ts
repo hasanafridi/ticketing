801350eb4d78e5ee294abbf5f5e18e6132ce7be0
Hasan Afridi <hasan.afridi@outlook.com>
2023-03-09 10:25:35 +0000

    create error for email in use

---
+++ auth/src/models/user.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+// an interface that describes the properties required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+//an interface that describes the properties a user model has
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//an interface that describes the properties a User Document has
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
