12b7c3db6e3f4000fd8e0940c64bffa0a12778ee
Hasan Afridi <hasan.afridi@outlook.com>
2023-03-16 19:09:33 +0000

    react signup and index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,17 @@
-const Landing = () => {
+import buildClient from "../api/build-client";
+
+const Landing = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
+};
+
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client
+    .get("/api/users/currentuser")
+    .catch((err) => console.log(err.message));
+  return data;
 };
 
 export default Landing;
