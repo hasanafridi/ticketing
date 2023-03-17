408addbd729629f0243471b74d138e86f36b0bd5
Hasan Afridi <hasan.afridi@outlook.com>
2023-03-17 15:26:28 +0000

    signout functionality

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,33 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+MyApp.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client
+    .get("/api/users/currentuser")
+    .catch((err) => console.log(err.message));
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx
+    ).catch((err) => console.log(err.message));
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
 export default MyApp;
