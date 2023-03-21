668360ce956717682c5d1817641e78670f7f33e3
Hasan Afridi <hasan.afridi@outlook.com>
2023-03-21 16:52:14 +0000

    TDD for new ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,9 +2,10 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 
-import { errorHandler, NotFoundError } from "@hkatickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@hkatickets/common";
 
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -15,6 +16,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
